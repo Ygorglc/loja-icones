2ff34c81306ed24486d7de2daa18dba898bd8772
Ygor Guilherme Lima Costa <ygorglc@gmail.com>
2026-06-18 20:29:10 -0300

    Update script.js
    atualizando o js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document
-    .getElementById("btnMensagem")
-    .addEventListener("click", () => {
-        alert("Funcionou!");
-    });
+const slides =
+    document.querySelectorAll('.slide');
+
+let indice = 0;
+
+setInterval(() => {
+
+    slides[indice]
+        .classList.remove('ativo');
+
+    indice++;
+
+    if(indice >= slides.length){
+        indice = 0;
+    }
+
+    slides[indice]
+        .classList.add('ativo');
+
+},3000);
