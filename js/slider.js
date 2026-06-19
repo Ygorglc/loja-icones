a766723b2fd75f553f86c2ab47be30170760f749
Ygor Guilherme Lima Costa <ygorglc@gmail.com>
2026-06-19 19:33:10 -0300

    Create slider.js
    criação do js para os slides

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+const slides =
+    document.querySelectorAll('.slide');
+
+let indice = 0;
+
+if (slides.length > 0) {
+
+    setInterval(() => {
+
+        slides[indice]
+            .classList.remove('ativo');
+
+        indice++;
+
+        if (indice >= slides.length) {
+            indice = 0;
+        }
+
+        slides[indice]
+            .classList.add('ativo');
+
+    }, 3000);
+
+}
